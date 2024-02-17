90591255ce802e1d23d5f8fe74affc7cbd5f2ff1
Valentina Manoli <valumanoli@gmail.com>
2024-02-17 15:50:24 -0300

    cambios br

--- Be Ready nuevo/script.js
+++ Be Ready nuevo/script.js
@@ -1,13 +1,10 @@
-// Obtener todos los elementos de la lista
 const itemsNosotros = document.querySelectorAll('.item-nosotros');
 
-// Función para verificar si un elemento es visible en la ventana
 function esElementoVisible(elemento) {
     const rect = elemento.getBoundingClientRect();
     return (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight));
 }
 
-// Función para mostrar elementos cuando son visibles
 function mostrarElementos() {
     itemsNosotros.forEach(item => {
         if (esElementoVisible(item)) {
@@ -16,8 +13,6 @@ function mostrarElementos() {
     });
 }
 
-// Escuchar el evento scroll para activar la función de mostrar elementos
 window.addEventListener('scroll', mostrarElementos);
 
-// Llamar a la función para mostrar elementos cuando la página se carga
 mostrarElementos();
